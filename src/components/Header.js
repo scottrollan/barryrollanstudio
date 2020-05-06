640a34478799f88d7325d1b700ec36e7853e4796
Barry Rollan <barryrollan@macbook-pro.lan>
2020-05-06 17:57:27 -0400

    changed font family to rubik, capitalized menu buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,16 +8,16 @@ class Header extends React.Component {
     return (
       <div className="ui three item menu">
         <Link to="/" className="item">
-          Home
+          HOME
         </Link>
         <Link to="/services" className="item">
-          Services
+          SERVICES
         </Link>
         {/* <Link to="/color" className="item">
           O&amp;M Color
         </Link> */}
         <Link to="/contact" className="item">
-          Contact
+          CONTACT
         </Link>
       </div>
     );
