04015e1143418c0377cba14a82901d7dee0eb8ce
Barry Rollan <barryrollan@macbook-pro.lan>
2020-05-08 09:54:25 -0400

    added styling based on vh to contact page to make the sizing look right on safari mobile....hopefully

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,42 +2,50 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div
-      className="ui container"
-      style={{ display: 'flex', justifyContent: 'center' }}
-    >
+    <div className="ui container">
       <form
         className="ui form"
         name="contact"
         method="post"
-        style={{ marginBottom: '20px', width: '100%', maxWidth: '640px' }}
+        style={{
+          width: '100%',
+          padding: '0 calc(50% - 320px',
+        }}
       >
         <div className="field">
           <input type="hidden" name="form-name" value="contact" />
-          <label>
+          <label style={{ fontSize: '1.8vh' }}>
             Name
-            <input type="text" name="name" placeholder="Name" required />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+              style={{ height: '4.8vh' }}
+            />
           </label>
         </div>
         <div className="field">
-          <label>
+          <label style={{ fontSize: '1.8vh' }}>
             E-mail
             <input
               type="email"
               name="email"
               placeholder="me@email.com"
               required
+              style={{ height: '4.8vh' }}
             />
           </label>
         </div>
         <div className="field">
-          <label>
+          <label style={{ fontSize: '1.8vh' }}>
             Message
             <textarea
               type="textarea"
               name="message"
               placeholder="Enter text..."
               required
+              style={{ height: '20.5vh' }}
             />
           </label>
         </div>
