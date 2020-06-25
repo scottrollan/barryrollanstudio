42d64d9a9ced6215ef084e9aecec479d8e1da726
Barry Rollan <barryrollan@macbook-pro.lan>
2020-06-25 07:50:55 -0400

    reconfigured contact form to give a success popup on submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,62 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import styles from './Contact.module.css';
+import AlertMessageSent from './popup/AlertMessageSent';
+import $ from 'jquery';
 
-const Contact = () => {
+function Contact() {
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const encode = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k) => {
+      formData.append(k, data[k]);
+    });
+    return formData;
+  };
+
+  const handleSubmit = (e) => {
+    const data = { 'form-name': 'contact', name, email, message };
+
+    fetch('/', {
+      method: 'POST',
+      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
+      body: encode(data),
+    })
+      .then(() => {
+        setStatus('Form Submission Successful!!');
+        $('#alertMessageSent').css('display', 'flex');
+        $('#alertMessageSent').delay(1500).fadeOut(1000);
+        setName('');
+        setEmail('');
+        setMessage('');
+        $('#contactForm')[0].reset();
+      })
+      .catch((error) => {
+        setStatus('Form Submission Failed!');
+        console.log(error);
+        alert(status);
+      });
+
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      return setName(value);
+    }
+    if (name === 'email') {
+      return setEmail(value);
+    }
+    if (name === 'message') {
+      return setMessage(value);
+    }
+  };
+
   return (
-    <div className="ui container">
-      <form
-        className="ui form"
-        name="contact"
-        method="post"
-        style={{
-          width: '100%',
-          padding: '0 calc(50% - 320px',
-        }}
-        action="/"
+    <Form
+      onSubmit={handleSubmit}
+      action="/thank-you/"
+      style={{
+        width: '100%',
+        padding: '0 calc(50% - 320px',
+      }}
+      id="contactForm"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <AlertMessageSent />
+
+      <div className={styles.nameEmailRow}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          placeholder="Your name"
+          required
+        />
+
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          placeholder="Your email"
+          required
+        />
+      </div>
+
+      <textarea
+        name="message"
+        value={message}
+        onChange={handleChange}
+        rows="10"
+        placeholder="Your message..."
+        required
+      />
+      {/* </Form.Group> */}
+      <Button
+        variant="secondary"
+        className={styles.contactButton}
+        type="submit"
       >
-        <div className="field">
-          <input type="hidden" name="form-name" value="contact" />
-          <label style={{ fontSize: '1.8vh' }}>
-            Name
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              required
-              style={{ height: '4.8vh', minHeight: '36px' }}
-            />
-          </label>
-        </div>
-        <div className="field">
-          <label style={{ fontSize: '1.8vh', height: 'auto' }}>
-            E-mail
-            <input
-              type="email"
-              name="email"
-              placeholder="me@email.com"
-              required
-              style={{ height: '4.8vh', minHeight: '36px' }}
-            />
-          </label>
-        </div>
-        <div className="field">
-          <label style={{ fontSize: '1.8vh' }}>
-            Message
-            <textarea
-              type="textarea"
-              name="message"
-              placeholder="Enter text..."
-              required
-              style={{ height: '20.5vh' }}
-            />
-          </label>
-        </div>
-
-        <button className="ui button" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+        Send
+      </Button>
+    </Form>
   );
-};
-
+}
 export default Contact;
