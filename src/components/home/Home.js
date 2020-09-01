fbd231fed66aad20a9facaf4572e18a12f93ffbc
Barry Rollan <barryrollan@macbook-pro.lan>
2020-09-01 18:27:01 -0400

    added weekly and monthly dynamic banners from simply organic

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import owayTop from '../../media/oway-banner-top.jpg';
 import oway from '../../media/oway-banner.jpg';
-import logo from '../../media/o&mLogo.png';
+// import logo from '../../media/o&mLogo.png';
 import om from '../../media/om_haircare.jpg';
 import styles from './Home.module.css';
 
@@ -13,8 +13,18 @@ const Home = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src={owayTop} alt="" className={styles.fader} />
-        <img src={oway} alt="" className={styles.solid} />
+        <img
+          src={owayTop}
+          alt=""
+          className={styles.fader}
+          style={{ width: '100%' }}
+        />
+        <img
+          src={oway}
+          alt=""
+          className={styles.solid}
+          style={{ padding: 0 }}
+        />
       </a>
       <p className={styles.blurb}>
         Healthy hair begins when it’s free from stress. Reduce the chemical
@@ -23,9 +33,16 @@ const Home = () => {
         chemicals wherever possible while including natural extracts and active
         minerals that deliver real benefits.
       </p>
-      <img src={logo} alt="" />
-
-      <img src={om} alt="" style={{ width: '100%' }} />
+      {/* <img src={logo} alt="" style={{ padding: 0 }} /> */}
+      <div className={styles.saleWrapper}>
+        <span> Weekly Online Sale</span>
+        <div id="rfsn_img_70361"></div>
+      </div>
+      <div className={styles.saleWrapper}>
+        <span> Monthly Online Sale</span>
+        <div id="rfsn_img_68795"></div>
+      </div>
+      <img src={om} alt="" style={{ padding: 0 }} />
     </div>
   );
 };
